f47b7649459521d9f455d5c50683e5c55a968a4a
abirpuc <mhabirbd2016@gmail.com>
2023-07-04 09:02:38 +0600

    design hero

--- src/components/sharecomponents/Hero.jsx
+++ src/components/sharecomponents/Hero.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div  className='text-white flex justify-center items-center max-w-[100vh]'>
-          <div className="text-center max-w-[880px] mt-[150px] w-full h-screen lg:mx-auto flex flex-col">
-            <p className='text-[#00df9a] text-3xl font-bold'>Growing With Data Analytics</p>
-            <h1 className='mt-2 text-xl'>Grow with Data.</h1>
+        <div  className='text-white'>
+          <div className="text-center h-screen w-full mx-auto flex flex-col mt-28">
+            <p className='text-[#00df9a] text-xl font-bold'>Growing With Data Analytics</p>
+            <h1 className='mt-2 md:text-7xl sm:text-6xl text-4xl font-bold md-py-4'>Grow with Data.</h1>
             <div className='mt-2 text-xl'>
-                Fast, flexible financing for
+                <p>Fast, flexible financing for</p>
+                
             </div>
+            <p className='mt-2 text-xl text-gray-500 font-bold'>Monitor your data analytics to increase revenue for BTB, BTC & SASS platforms.</p>
+            <button className='mt-3 uppercase -tracking-2 bg-[#00df9a] p-2 w-[200px] rounded-md mx-auto hover:bg-[#10a87b] font-bold'>Get Start</button>
           </div>
         </div>
     );
